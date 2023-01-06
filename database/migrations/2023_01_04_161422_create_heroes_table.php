7c2d1bfa34daff6873684fb0e7490215ab7b001b
i_ilyukha_runByIt <i.ilyukha@runbyit.com>
2023-01-06 13:23:03 +0200

    Create heroes models/migrations

---
+++ database/migrations/2023_01_04_161422_create_heroes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('heroes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->string('name');
+            $table->float('strength');
+            $table->float('agility');
+            $table->float('intelligence');
+            $table->float('range');
+            $table->float('avatar');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('heroes');
+    }
+};
